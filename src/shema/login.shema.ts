8a98730cb3f226cd60e3a221723395e2fad33f72
abdalmlk <dbalkmal7@gmail.com>
2026-02-24 17:01:18 +0200

    new change

--- src/shema/login.shema.ts
+++ src/shema/login.shema.ts
@@ -1,12 +1,28 @@
-import z from "zod";
+// import z from "zod";
 
-export const loginshema=z.object({
+// export const loginshema=z.object({
     
-    email:z.email("Invalid email address").nonempty("Email is required"),
-    password:z.string().nonempty("Password is required").min(6,"Password must be at least 6 characters long").max(20,"Password must be at most 20 characters long"),
+//     email:z.email("Invalid email address").nonempty("Email is required"),
+//     password:z.string().nonempty("Password is required").min(6,"Password must be at least 6 characters long").max(20,"Password must be at most 20 characters long"),
  
 
 
-})
+// })
 
-export type loginType = z.infer<typeof loginshema>;
+// export type loginType = z.infer<typeof loginshema>;
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .nonempty("Email is required")
+    .email("Invalid email address"),
+
+  password: z
+    .string()
+    .nonempty("Password is required")
+    .min(6, "Password must be at least 6 characters long")
+    .max(20, "Password must be at most 20 characters long"),
+});
+
+export type LoginType = z.infer<typeof loginSchema>;
